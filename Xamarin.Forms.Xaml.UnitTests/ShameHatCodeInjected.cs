12ebec85123f4056c5456e3661726c9e0eae994b
Stephane Delcroix <stephane@delcroix.org>
2018-05-31 10:11:27 +0200

    [build] post-build fix of the assembly
    - fixes #2835

---
+++ Xamarin.Forms.Xaml.UnitTests/ShameHatCodeInjected.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+	[TestFixture]
+	public class ShameHatCodeInjected
+	{
+		[Test]
+		public void InjectedCodeIsPresent()
+		{
+			var resLoader = typeof(Xamarin.Forms.Internals.ResourceLoader);
+			Assert.True(resLoader.GetMethods().Any(mi => mi.Name == "get_ResourceProvider" && mi.ReturnType == typeof(Func<string,string>)));
+
+		}
+	}
+}
